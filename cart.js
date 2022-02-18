04fb000e7f5422a7aaa822e1762056151352856c
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-18 10:01:10 -0700

    Problem 1 complete.

--- cart.js
+++ cart.js
@@ -35,7 +35,9 @@ const cart = [
 
 //CODE HERE
 
-// const summedPrice = cart.reduce(/* CALLBACK HERE */)
+const summedPrice = cart.reduce((sum, {price}) => sum + price, 0);
+
+console.log(summedPrice)
 
 
 //////////////////PROBLEM 2////////////////////
